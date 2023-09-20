d50f3ae73d4141743e6328b980d31aacc1615949
Sourav-1710 <145518767+Sourav-1710@users.noreply.github.com>
2023-09-21 00:18:55 +1200

    adding colors

--- colorful_star.py
+++ colorful_star.py
@@ -14,3 +14,9 @@ s.bgcolor("black")
 
 # Set the turtle drawing speed to 5 (moderate speed)
 t.speed(5)
+
+# Define a list of colors for the star
+color = ['green', 'yellow', 'blue', 'cyan']
+
+# Initialize the color index
+c = 0
