640471452f7d0f9abf1191b9a78cf3244e55ce12
Sourav-1710 <145518767+Sourav-1710@users.noreply.github.com>
2023-09-21 00:16:54 +1200

    Starting it

---
+++ colorful_star.py
@@ -0,0 +1,6 @@
+# Import the turtle module
+import turtle
+
+# Create a turtle screen with dimensions 500x600, starting position (0, 100)
+screen = turtle.Screen()
+screen.setup(500, 600, startx=0, starty=100)
