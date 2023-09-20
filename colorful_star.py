bd1a47b6a2b08a89683aeaf3cfc888cebbe1b6fa
Sourav-1710 <145518767+Sourav-1710@users.noreply.github.com>
2023-09-21 00:18:14 +1200

    object, screen and speed set

--- colorful_star.py
+++ colorful_star.py
@@ -4,3 +4,13 @@ import turtle
 # Create a turtle screen with dimensions 500x600, starting position (0, 100)
 screen = turtle.Screen()
 screen.setup(500, 600, startx=0, starty=100)
+
+# Create a turtle object
+t = turtle.Turtle()
+
+# Create another turtle screen
+s = turtle.Screen()
+s.bgcolor("black")
+
+# Set the turtle drawing speed to 5 (moderate speed)
+t.speed(5)
